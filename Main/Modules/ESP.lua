8997f1f42ded9c074e650f2fe6dc5a35b9fbb451
AnderDevUI <andersson.ruiz.2006@gmail.com>
2024-05-30 11:05:27 -0500

    Update ESP.lua

--- Main/Modules/ESP.lua
+++ Main/Modules/ESP.lua
@@ -1,10 +1,10 @@
 local game = game
 local select, pcall, loadstring , warn = select, pcall, loadstring, warn
 
-local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))))
+local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/AnderDevUI/Anderson_Modules/main/Ander_ESP/Origin.lua"))))
 
 if not Success then
-    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))
+    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/AnderDevUI/Anderson_Modules/main/Ander_ESP/Support.lua"))))
 
     if not Success then
         return warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
